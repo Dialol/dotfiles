cb8902c580471679237693d4a90f8f2eedda787b
Dialol <Diabolik_sl@mail.ru>
2025-07-17 19:01:30 +0300

    add

--- config/.config/nvim/lua/plugins/buffline.lua
+++
@@ -1,7 +0,0 @@
-require("bufferline").setup{
-    options = {
-        close_command = function(bufnum)
-            require('mini.bufremove').delete(bufnum, false)
-        end,
-    }
-}
--- config/.config/nvim/lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
--- Настройка Mason
-require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = { "pyright", "bashls", "cssls", "html" },
-  automatic_installation = true,
-}
-
